8d922d20b7bbb0af2f966f5ff0110b6fd833cc78
Mohammad Alka. R <99939707+alfkarmdhn@users.noreply.github.com>
2022-04-04 19:12:04 +0700

    Create Hitung.java

---
+++ Hitung.java
@@ -0,0 +1,12 @@
+
+package bangunruang;
+
+public class Hitung {
+      public static void main(String[] args) {
+        BangunRuang hitunglah=new BangunRuang();
+        
+        hitunglah.volume(5, 8, 10);
+        hitunglah.volume(8);
+      }
+      
+}
